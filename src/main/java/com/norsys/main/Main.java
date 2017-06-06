413bfc60bbfcc7dd786fbb1be9ecbad1c25e142f
Fred M <frederic.maquet@gmail.com>
2017-06-06 22:20:48 +0200

    ajout Calculatrice

--- src/main/java/com/norsys/main/Main.java
+++ src/main/java/com/norsys/main/Main.java
@@ -1,8 +1,11 @@
 package com.norsys.main;
 
+import com.norsys.outils.Calculatrice;
+
 public class Main{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+	String helloWorld = "Hello World !";
+	Calculatrice calculatrice = new Calculatrice();
+        System.out.println( "Hello World! "+calculatrice.somme(1,1));
     }
 }
---
+++ src/main/java/com/norsys/outils/Calculatrice.java
@@ -0,0 +1,9 @@
+package com.norsys.outils;
+
+public class Calculatrice {
+
+ public static int somme(int a, int b) {
+  return a + b;
+ }
+
+}
